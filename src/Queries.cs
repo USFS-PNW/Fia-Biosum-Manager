772b78a8c80118ab38f28b662eb98199cb3e76d9
Tyler Sorg <tyler.sorg@usda.gov>
2021-07-21 16:36:43 -0700

    Removed an extra comma that made a query invalid

--- src/Queries.cs
+++ src/Queries.cs
@@ -3517,7 +3517,7 @@ namespace FIA_Biosum_Manager
                            "i.roughcull=IIF(t.roughcull IS NULL,0,t.roughcull)," +
                            "i.decaycd=IIF(t.decaycd IS NULL,0,t.decaycd)," +
                            "i.balive=c.balive," +
-                           "i.precipitation=p.precipitation,";
+                           "i.precipitation=p.precipitation";
                 }
 
                 public static string BuildInputTableForVolumeCalculation_Step3(
