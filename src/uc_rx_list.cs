430583cdd6720a9e458da5e35e12d287481d3b5e
lbross <lbross@pdx.edu>
2023-11-29 10:59:27 -0800

    issue #260: Another temp table to improve performance accessing data in FVSOut.db

--- src/uc_rx_list.cs
+++ src/uc_rx_list.cs
@@ -4059,6 +4059,20 @@ namespace FIA_Biosum_Manager
 
                 if (p_bAudit)
                 {
+                    // Create extract of p_strSourceTableName to improve performance
+                    const string TMPFVSOUT = "tmpFvsOut";
+                    if (dataMgr.TableExist(conn, TMPFVSOUT))
+                    {
+                        dataMgr.SqlNonQuery(conn, $@"drop table {TMPFVSOUT}");
+                    }
+                    dataMgr.m_strSQL = $@"CREATE TABLE {TMPFVSOUT} AS SELECT * FROM {p_strSourceTableName} s, {Tables.FVS.DefaultFVSCasesTempTableName} c
+                                        where s.CaseID = c.CaseID AND RunTitle = '{p_strRunTitle}'";
+                    if (p_bDebug && frmMain.g_intDebugLevel > 2)
+                        frmMain.g_oUtils.WriteText(p_strDebugFile, "START: " + System.DateTime.Now.ToString() + "\r\n" + dataMgr.m_strSQL + "\r\n");
+                    dataMgr.SqlNonQuery(conn, dataMgr.m_strSQL);
+                    if (p_bDebug && frmMain.g_intDebugLevel > 2)
+                        frmMain.g_oUtils.WriteText(p_strDebugFile, "DONE: " + System.DateTime.Now.ToString() + "\r\n");
+
                     //
                     //stand + year rowcounts for all tables as compared with stand + year in the summary table
                     //
@@ -4069,7 +4083,7 @@ namespace FIA_Biosum_Manager
                     string[] strSQL = Queries.FVS.FVSOutputTable_AuditFVSSummaryTableRowCountsSQL(
                         "temp_rowcount",
                         "audit_FVS_SUMMARY_year_counts_table",
-                        p_strSourceTableName,
+                        TMPFVSOUT,
                         p_strSourceTableName, p_strRunTitle);
 
                     for (x = 0; x <= strSQL.Length - 1; x++)
@@ -4079,6 +4093,17 @@ namespace FIA_Biosum_Manager
                         dataMgr.SqlNonQuery(conn, strSQL[x]);
                         if (p_bDebug && frmMain.g_intDebugLevel > 2)
                             frmMain.g_oUtils.WriteText(p_strDebugFile, "DONE: " + System.DateTime.Now.ToString() + "\r\n");
+                    }
+
+                    // Clean up tmp table
+                    if (dataMgr.TableExist(conn, TMPFVSOUT))
+                    {
+                        if (p_bDebug && frmMain.g_intDebugLevel > 2)
+                            frmMain.g_oUtils.WriteText(p_strDebugFile, "START: " + System.DateTime.Now.ToString() + "\r\n" + $@"drop table {TMPFVSOUT}" + "\r\n");
+                        dataMgr.SqlNonQuery(conn, $@"drop table {TMPFVSOUT}");
+                        if (p_bDebug && frmMain.g_intDebugLevel > 2)
+                            frmMain.g_oUtils.WriteText(p_strDebugFile, "DONE: " + System.DateTime.Now.ToString() + "\r\n");
+
                     }
                 }
 
